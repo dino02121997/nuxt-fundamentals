4740f1b4edb6fef70178c5b1569ccf6dc1bf1f90
TA TY TY <tytt@TAs-MacBook-Pro.local>
2023-06-12 14:23:51 +0700

    usingFetch

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -8,17 +8,13 @@
 
 <script setup>
 const route = useRoute();
-const { data } = useAsyncData(`/movies/${route.params.id}`,() => {
-  return $fetch(`http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`);
-},{
-    pick : ["Plot","Title"]
-    // transform(data) {
-    //     return { 
-    //         Plot : data.Plot,
-    //         Title : data.Title
-    //     }
-    // }
-});
+const { data } = useFetch(
+  `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
+  {
+    pick: ["Plot", "Title"],
+    key: `/movies/${route.params.id}`,
+  }
+);
 </script>
 
 <style>
