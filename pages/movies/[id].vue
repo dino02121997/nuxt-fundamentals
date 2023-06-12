3bef1d38abde3779db8b9f2c207a62754f8a4c71
TA TY TY <tytt@TAs-MacBook-Pro.local>
2023-06-12 14:40:35 +0700

    handle errror

--- pages/movies/[id].vue
+++ pages/movies/[id].vue
@@ -10,9 +10,18 @@
 const route = useRoute();
 const { data } = useFetch(
   `http://www.omdbapi.com/?apikey=8e3f600b&i=${route.params.id}`,
+//     `https://httpbin.org/status/500`,
   {
     pick: ["Plot", "Title"],
     key: `/movies/${route.params.id}`,
+    onResponse({ request, response }) {
+      if (response._data.Error === "Incorrect IMDb ID.") {
+        showError({ statusCode: 404, statusMessage: "Page Not Found" });
+      }
+    },
+    onResponseError(response) {
+        showError({ statusCode: 500, statusMessage: "Internal Server Error" });
+    },
   }
 );
 </script>
