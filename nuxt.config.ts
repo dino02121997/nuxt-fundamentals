115757dfa0d11760db0e36afefb2abfc8ba3009c
TA TY TY <tytt@TAs-MacBook-Pro.local>
2023-06-13 10:55:36 +0700

    update nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,7 @@ export default defineNuxtConfig({
   ,
   routeRules: {
     "/hybrid/spa": { ssr: false },
-    "/hybrid/static": { static: true },
+    "/hybrid/static": { prerender: true },
     "/hybrid/swr": { swr: true },
   },
 
